f814b5015ce5cd0d010cfa2d605a51160a3a4877
yunusemredilber <yunusemredilber@yandex.com>
2021-03-02 21:03:49 +0300

    Fix color hierarchy issue for dark theme

--- gatsby-config.js
+++ gatsby-config.js
@@ -43,7 +43,7 @@ module.exports = {
       resolve: `gatsby-plugin-google-fonts`,
       options: {
         fonts: [
-          'Playfair+Display:400,700'
+          'Montserrat:400,700'
         ],
         display: 'swap'
       }
